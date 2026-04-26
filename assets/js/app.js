c985fb6933db4e14efed012a91ff068bf4ad8f51
Valerii Trufanov <valeriy.trufanov@mirapolis.ru>
2026-04-26 18:57:21 +0300

    03 - Корректное отображение сайта

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ import { initI18n } from "./i18n.js";
 import { initAnimations } from "./animations.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  document.documentElement.classList.add("animations-ready");
   initTheme();
   initI18n();
   initAnimations();
